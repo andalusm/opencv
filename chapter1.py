032c42befd94ef8e95f240b140365bbcd9d011e7
andalusm <andalos2007@hotmail.com>
2020-07-07 02:31:35 +0300

    Added the ability to read from videos

--- chapter1.py
+++ chapter1.py
@@ -9,8 +9,27 @@ import cv2
 img = cv2.imread("resources/lena.png")
 
 #we need to show the image (takes name of window and img)
-cv2.imshow("Output2",img)
+cv2.imshow("Output1",img)
 
 #it goes away quickly we need to add deley
 # 0=inf and everything else is how many mili second
 cv2.waitKey(0)
+
+#==============================
+##========Read Video===========
+#==============================
+
+#we need to read a video
+vid = cv2.VideoCapture("resources/test_video.mp4")
+
+#to read the video we have to save each image of it alone and run one by one
+while True:
+    #return sucess if it read the image and save this image frame in a variable
+    success, img = vid.read()
+    # we need to show the image frame (takes name of window and img)
+    cv2.imshow("Output2", img)
+    #add delay to img for each frame and if clicked q it exits
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
