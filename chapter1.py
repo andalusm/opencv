4369eadf15fa0b710e4d9377e4597b2dbeb76c8f
andalusm <andalos2007@hotmail.com>
2020-07-07 02:17:57 +0300

    Added the abbility to read from images

--- chapter1.py
+++ chapter1.py
@@ -1,3 +1,16 @@
 import cv2
 
-print("Package imported")
+#==============================
+##========Read Image===========
+#==============================
+
+
+#We need to read an image
+img = cv2.imread("resources/lena.png")
+
+#we need to show the image (takes name of window and img)
+cv2.imshow("Output2",img)
+
+#it goes away quickly we need to add deley
+# 0=inf and everything else is how many mili second
+cv2.waitKey(0)
