4dd4754f63369964bb09056547deca4bc715a29d
andalusm <andalos2007@hotmail.com>
2020-07-07 18:15:36 +0300

    added edges to image

--- chapter2.py
+++ chapter2.py
@@ -8,8 +8,11 @@ img = cv2.imread("resources/lena.png")
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 #Blur it takes image the scale (has to be odd numbers) and sigmaX
 imgBlur = cv2.GaussianBlur(img,(7,7),0)
+#Canny finds edges in image add threshold
+imgCanny = cv2.Canny(img,100,100)
 
 
 cv2.imshow("Gray image",imgGray)
 cv2.imshow("Blur image",imgBlur)
+cv2.imshow("Canny image",imgCanny)
 cv2.waitKey(0)
