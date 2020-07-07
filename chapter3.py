0c49937e8be08159661e0b1c3f0e3c4b0e3b1e0f
andalusm <andalos2007@hotmail.com>
2020-07-07 23:58:24 +0300

    added cropping

--- chapter3.py
+++ chapter3.py
@@ -7,11 +7,16 @@ img = cv2.imread("resources/lambo.png")
 print(img.shape)
 
 #resizing put width then height (we can increase or decrease the size)
-imgResize = cv2.resize(img,(300,200))
+imgResize = cv2.resize(img,(1000,500))
 print(imgResize.shape)
+
+#Cropping image height first then with
+imgCrop = img[0:200,200:500]
+
 
 cv2.imshow("Image",img)
 cv2.imshow("Image Resized",imgResize)
+cv2.imshow("Image Cropped",imgCrop)
 
 
 cv2.waitKey(0)
