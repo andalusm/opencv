95aaa1152cca4c9b7e622f1368f12192559627ca
andalusm <andalos2007@hotmail.com>
2020-07-07 18:11:29 +0300

    added blur to image

--- chapter2.py
+++ chapter2.py
@@ -6,7 +6,10 @@ img = cv2.imread("resources/lena.png")
 #convert the image into
 #Grayscale
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#Blur it takes image the scale (has to be odd numbers) and sigmaX
+imgBlur = cv2.GaussianBlur(img,(7,7),0)
 
 
 cv2.imshow("Gray image",imgGray)
+cv2.imshow("Blur image",imgBlur)
 cv2.waitKey(0)
