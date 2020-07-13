17a0bcd82a27094959f6e0c18963658776ef928a
andalusm <andalos2007@hotmail.com>
2020-07-13 23:19:38 +0300

    detect and show color based on the input

--- Project1.py
+++ Project1.py
@@ -11,10 +11,15 @@ cap.set(10,150)
 myColors = [[0,111,0,9,255,255],
             [29,24,0,104,88,117]]
 
+#The colors in BGR
+myColorValues = [[0,0,205],
+                 [0,51,0]]
+
 #this function should find colors and detect them and show them via mask
-def findColor(img,myColors):
+def findColor(img,myColors,myColorValues):
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     #added to detect all colors
+    count=0
     for color in myColors:
         lower = np.array(color[0:3])
         upper = np.array(color[3:6])
@@ -22,8 +27,8 @@ def findColor(img,myColors):
         #send mask to get contours and return middle point
         x,y=getContours(mask)
         #draw circle on the image result
-        cv2.circle(imgResult,(x,y),10,(255,255,255),cv2.FILLED)
-
+        cv2.circle(imgResult,(x,y),10,myColorValues[count],cv2.FILLED)
+        count+=1
         # cv2.imshow(str(color),mask)
 
 #we need to approximate the bounding box around it
@@ -34,7 +39,7 @@ def getContours(img):
     for cnt in contours:
         area = cv2.contourArea(cnt)
         if area > 500:
-            cv2.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
+            #cv2.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
             peri = cv2.arcLength(cnt, True)
             approx = cv2.approxPolyDP(cnt, 0.02*peri,True)
             x, y, w, h = cv2.boundingRect(approx)
@@ -46,7 +51,7 @@ def getContours(img):
 while True:
     success, img = cap.read()
     imgResult = img.copy()
-    findColor(img, myColors)
+    findColor(img, myColors,myColorValues)
     #we want to display it with contour
     cv2.imshow("Capture", imgResult)
     if cv2.waitKey(1) & 0xFF == ord('q'):
