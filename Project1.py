72745015965c29fbf1415da74aa6eb6f1b0faac4
andalusm <andalos2007@hotmail.com>
2020-07-13 23:06:56 +0300

    detects certain color and puts a  bounding bar around it

--- Project1.py
+++ Project1.py
@@ -19,13 +19,28 @@ def findColor(img,myColors):
         lower = np.array(color[0:3])
         upper = np.array(color[3:6])
         mask = cv2.inRange(imgHSV, lower, upper)
-        cv2.imshow(str(color),mask)
+        #send mask to get contours
+        getContours(mask)
+        # cv2.imshow(str(color),mask)
+
+#we need to approximate the bounding box around it
+def getContours(img):
+    contours , hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 500:
+            cv2.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
+            peri = cv2.arcLength(cnt, True)
+            approx = cv2.approxPolyDP(cnt, 0.02*peri,True)
+            x, y, w, h = cv2.boundingRect(approx)
 
 
 
 while True:
     success, img = cap.read()
+    imgResult = img.copy()
     findColor(img, myColors)
-    cv2.imshow("Capture", img)
+    #we want to display it with contour
+    cv2.imshow("Capture", imgResult)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
