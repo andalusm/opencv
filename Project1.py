59cf842d9c3ef9151fea2f82cb2cce31ca5e459f
andalusm <andalos2007@hotmail.com>
2020-07-13 22:54:23 +0300

    detected first color and added a color picker

--- Project1.py
+++ Project1.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 
 #we take the code from  the webcam in chapter 1
@@ -6,9 +7,23 @@ cap = cv2.VideoCapture(0)
 cap.set(3,640)
 cap.set(4,480)
 cap.set(10,150)
+#List of colors we want to detect we found them using Color picker
+myColors = [[0,111,0,9,255,255],
+            [29,24,0,104,88,117]]
+
+#this function should find colors and detect them and show them via mask
+def findColor(img,myColors):
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower = np.array(myColors[0][0:3])
+    upper = np.array(myColors[0][3:6])
+    mask = cv2.inRange(imgHSV, lower, upper)
+    cv2.imshow("img",mask)
+
+
 
 while True:
     success, img = cap.read()
+    findColor(img, myColors)
     cv2.imshow("Capture", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
